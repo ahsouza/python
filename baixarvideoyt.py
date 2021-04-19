1ab81b7c06bd973b25ad7eb349a7ebfbfa47e209
William <willsantos96@gmail.com>
2021-04-19 08:56:53 -0300

    python

--- baixarvideoyt.py
+++
@@ -1,7 +0,0 @@
-import pytube
-
-url = 'https://www.youtube.com/watch?v=KuVAeTHqijk'
-youtube = pytube.YouTube(url)
-video = youtube.streams.get_highest_resolution()
-
-video.download('/home/william/Downloads')
--- ytdownloader.py
+++ ytdownloader.py
@@ -1,6 +1,6 @@
 import pytube
 
-url = 'https://www.youtube.com/watch?v=KuVAeTHqijk'
+url = 'LINK DO VIDEO DO YOUTUBE'
 youtube = pytube.YouTube(url)
 video = youtube.streams.get_highest_resolution()
 
